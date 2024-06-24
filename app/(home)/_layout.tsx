396883c1ea0df067eb68d2524391757f885915bb
mrDezz <deepakguptaswork@gmail.com>
2024-06-25 03:52:48 +0530

    profile picture

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -3,14 +3,15 @@ import ChatProvider from "@/components/providers/chatProvider";
 import { Redirect, Stack } from "expo-router";
 
 export default function HomeLayout() {
-    const { user } = useAuth();
-    if (!user) {
-      return <Redirect href="/(auth)" />;
-    }
+  const { user } = useAuth();
+  if (!user) {
+    return <Redirect href="/(auth)" />;
+  }
   return (
     <ChatProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
-    </ChatProvider> );
+    </ChatProvider>
+  );
 }
