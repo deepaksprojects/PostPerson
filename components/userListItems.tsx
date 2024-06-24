430902dde45001c0093396ac5770b7c0eb84fced
mrDezz <deepakguptaswork@gmail.com>
2024-06-25 04:58:35 +0530

    start chnnel with a user

--- components/userListItems.tsx
+++ components/userListItems.tsx
@@ -1,11 +1,27 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { Pressable, Text } from "react-native";
+import { useChatContext } from "stream-chat-expo";
+import { useAuth } from "./providers/authProvider";
+import { router } from "expo-router";
 
 const UserListItems = ({ user }: any) => {
+  const { client } = useChatContext();
+  const { user: me } = useAuth();
+  const onPress = async () => {
+    const channel = client.channel("messaging", {
+      members: [me?.id, user.id],
+    });
+    channel.watch();
+    router.replace(`/(home)/channel/${channel.cid}`);
+  };
+
   return (
-    <View style={{padding:10,backgroundColor:'white',}} >
-      <Text style={{fontWeight:'600'}} >{user.full_name ?? "No Name"}</Text>
-    </View>
+    <Pressable
+      onPress={onPress}
+      style={{ padding: 10, backgroundColor: "white" }}
+    >
+      <Text style={{ fontWeight: "600" }}>{user.full_name ?? "No Name"}</Text>
+    </Pressable>
   );
 };
 
