0452019072b6e4b01805dc702d30a33c78206878
mrDezz <deepakguptaswork@gmail.com>
2024-06-24 22:13:26 +0530

    channel added

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -1,20 +1,6 @@
-import { StyleSheet } from "react-native";
-
-import { Channel as ChannelType } from "stream-chat";
-
-import { useState } from "react";
-import { Channel, ChannelList, MessageInput, MessageList } from "stream-chat-expo";
 import { router } from "expo-router";
+import { ChannelList } from "stream-chat-expo";
 
 export default function MainTabScreen() {
-  const [channel, setChannel] = useState<ChannelType>();
-
-  if (channel) {
-    return <Channel channel={channel}>
-      <MessageList />
-      <MessageInput />
-    </Channel>;
-  }
-
-  return <ChannelList onSelect={(channel) => router.push('/channel')} />;
+  return <ChannelList onSelect={(channel) => router.push(`/channel/${channel?.cid}`)} />;
 }
