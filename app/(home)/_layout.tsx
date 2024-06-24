3c4b3699949ccb1ceb6f3aa747beeed737e6e453
mrDezz <deepakguptaswork@gmail.com>
2024-06-25 02:44:12 +0530

    clean coode

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,39 +1,11 @@
+import ChatProvider from "@/components/providers/chatProvider";
 import { Stack } from "expo-router";
-import { useEffect } from "react";
-import { StreamChat } from "stream-chat";
-import { Chat, OverlayProvider } from "stream-chat-expo";
-
-const client = StreamChat.getInstance("dm7xzvv2fhb9");
 
 export default function HomeLayout() {
-  useEffect(() => {
-    const connect = async () => {
-      await client.connectUser(
-        {
-          id: "jlahey",
-          name: "Jim Lahey",
-          image: "https://i.imgur.com/fR9Jz14.png",
-        },
-        client.devToken("jlahey")
-      );
-
-      //   const channel = client.channel("messaging", "the_park", {
-      //     name: "The Park",
-
-      // });
-      // await channel.watch()
-    };
-
-    connect();
-  }, []);
-
   return (
-    <OverlayProvider>
-      <Chat client={client}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </Chat>
-    </OverlayProvider>
-  );
+    <ChatProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ChatProvider> );
 }
